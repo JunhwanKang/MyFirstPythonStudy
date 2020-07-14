4a8c6fb2775388d7653cd65737cb0a6927ea736c
JunhwanKang <wnsghks1017@naver.com>
2020-07-14 16:35:43 +0900

    중복 허용X

--- 당첨자뽑기.py
+++ 당첨자뽑기.py
@@ -10,9 +10,11 @@
 
 from random import *
 
+users = range(1,21)
+users = list(users)
+#print(type(users))
+winners = sample(users, 4)
 print("-- 당첨자 발표 --")
-chicken = int(random() * 20) +1
-print("치킨 당첨자 : "+str(chicken))
-coffee = [int(random() * 20) +1,int(random() * 20) +1,int(random() * 20) +1]
-print("커피 당첨자 : "+str(coffee))
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
 print("-- 축하합니다 --")
