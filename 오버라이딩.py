8cd3285d8e8be440d349e349edc4b1ecb0dbec6f
JunhwanKang <wnsghks1017@naver.com>
2020-07-18 03:36:22 +0900

    건물 유닛 추가, pass 개념 체크, super개념 체크

--- 오버라이딩.py
+++ 오버라이딩.py
@@ -60,3 +60,20 @@ battlecruiser = FlyAttackUnit("배틀크루져", 500, 25 ,3)
 
 valture.move("11시")
 battlecruiser.move("9시")
+
+#건물 
+class bulidingUnit(Unit):
+    def __init__(self, name, hp, location):
+        # Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0)
+
+supply_depot = bulidingUnit("서플라이 디폿", 500, "7시")    
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    pass #아무것도 안하고 넘어간다 (완성된것 처럼)
+
+game_start()
+game_over()
