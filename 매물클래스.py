672576ef73980520a7bf4e63e2e53c6c32d8bdbe
JunhwanKang <wnsghks1017@naver.com>
2020-07-19 20:36:20 +0900

    클래스를 이용한 예제

---
+++ 매물클래스.py
@@ -0,0 +1,30 @@
+# 부동산 매물 프로그램을 작성하시오.
+
+# 총3대의 매물이 있습니다.
+#강남 아파트 매매 10억 2010년
+#마포 오피스텔 전세 5억 2007년
+#송파 빌라 월세 500/50 2000년
+
+class house:
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
+
+    def show_detail(self):
+        print(self.location,self.house_type,self.deal_type,self.price,self.completion_year)
+
+homes = []
+home1 = house("강남", "아파트", "매매", "10억", "2010년")
+home2 = house("마포", "오피스텔", "전세", "5억", "2007년")
+home3 = house("송파", "빌라", "월세", "500/50", "2000년")
+
+homes.append(home1)
+homes.append(home2)
+homes.append(home3)
+
+print("총 {}대의 매물이 있습니다.".format(len(homes)))
+for home in homes:
+    home.show_detail()
