ce5ae9d79b217cb79e23b7a5c6ae0b6320549027
JunhwanKang <wnsghks1017@naver.com>
2020-07-20 18:02:22 +0900

    모듈을 이용한 간단한 예제

---
+++ byme.py
@@ -0,0 +1,4 @@
+def sign(name):
+    print("이 프로그램은 {0}에 의해 만들어졌습니다.".format(name))
+    print("Tel : 010-1234-5678")
+    print("이메일 :  user@OOOOO.com")
---
+++ 모듈예제.py
@@ -0,0 +1,15 @@
+#프로젝트 내에 나만의 시그니처를 남기는 모듈을 만들어보자!
+
+#조건: 모듈 파일명은 byme.py로 작성
+
+#(사용 예제)
+#import byme
+#byme.sign()
+
+#(출력 예제)
+#이 프로그램은 OOO에 의해 만들어졌습니다.
+#Tel : 010-1234-5678
+#이메일 : user@OOOOO.com
+
+import byme
+byme.sign("강준환")
